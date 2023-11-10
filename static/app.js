c8712c9a33dcb459e48e09e8634fe8300981e473
m <63015852+0mlml@users.noreply.github.com>
2023-11-11 01:32:40 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -43,6 +43,7 @@ const game = {
     /**
      * @description Requests a new round from the server
      * @param {number} wager The amount of the wager
+     * @throws {Error} If the server returns an error
      */
     requestRound: async (wager) => {
         const round = await fetch("/play", {
@@ -53,7 +54,9 @@ const game = {
             body: JSON.stringify({ amount: wager })
         }).then(async response => await response.json());
 
-        if (round.error) throw new Error(round.error);
+        if (round.error) {
+            throw new Error(round.error);
+        }
 
         game.currentRound = {
             chainStartPoint: round.chainStartPoint,
@@ -89,31 +92,35 @@ const GUI = {
     texture: null,
     // The text block for the multiplier
     multiplierBlock: null,
-
     // The current tiles
     tiles: [],
     /**
      * @description Renders the tiles
      * @param {number} dim The dimension of the grid of tiles
-     */ 
+     */
     renderTiles: (dim) => {
         const onTileClick = (tile) => {
-            GUI.tiles.splice(GUI.tiles.indexOf(tile), 1);
-            
+            const tilesNotClicked = GUI.tiles.filter(t => t !== tile);
+
             const roll = game.incrementRound();
 
-            // TODO: Place the roll # on the tile.
-            tile.text = roll.roll;
+            tile.textBlock.text = `x${roll.roll.toFixed(2)}`;
             tile.onPointerUpObservable.clear();
 
-            // TODO: Generate random roll #s for the other tiles.
-            for (const otherTile of GUI.tiles) {
+            for (const otherTile of tilesNotClicked) {
+                otherTile.textBlock.text = `x${((Math.random() - 0.3) * -2).toFixed(2)}`
                 otherTile.onPointerUpObservable.clear();
             }
 
-            // TODO: Update the multiplier block.
             GUI.multiplierBlock.text = `x${roll.newMultiplier.toFixed(2)}`;
 
+            if (roll.didExplode) {
+                alert(`gg pal`);
+            } else if (game.currentRound.rollsIndex >= game.currentRound.rolls.length) {
+                alert(`You won ${game.currentRound.net.toFixed(2)}!`);
+            } else {
+                GUI.nextRoundButton.isVisible = true;
+            }
         }
 
         for (let i = 0; i < dim * dim; i++) {
@@ -124,16 +131,67 @@ const GUI = {
             tile.background = "grey";
             tile.thickness = 0;
             tile.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+            tile.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
 
-            tile.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT + i % 2;
-            tile.left = i % 2 === 0 ? "30%" : "70%";
-            tile.top = i < 2 ? "-50px" : "50px";
+            tile.left = i % 2 === 0 ? "-50px" : "50px";
+            tile.top = i < 2 ? "90px" : "190px";
 
             tile.onPointerUpObservable.add(() => onTileClick(tile));
 
             GUI.texture.addControl(tile);
             GUI.tiles.push(tile);
         }
+    },
+    // The current wager block
+    wagerBlock: null,
+    // The play button
+    playButton: null,
+    /**
+     * @description Handles the play button click
+     */
+    onPlayButtonClick: async () => {
+        const wager = parseFloat(GUI.wagerBlock.text);
+        if (isNaN(wager) || wager <= 0) return;
+
+        let round = null;
+        try {
+            round = await game.requestRound(wager);
+        } catch (err) {
+            console.error(err);
+            alert(`An error occurred: ${err.message}`);
+            return;
+        }
+
+        // Hide the wager block and play button
+        GUI.wagerBlock.isVisible = false;
+        GUI.playButton.isVisible = false;
+
+        // Update multiplier block
+        GUI.multiplierBlock.text = `x0.0`;
+
+        GUI.guiGoNext();
+    },
+    /**
+     * @description Handles starting the next round
+     */
+    guiGoNext: () => {
+        // Kill the last round's tiles
+        for (const tile of GUI.tiles) {
+            GUI.texture.removeControl(tile);
+        }
+
+        // Render the tiles
+        GUI.renderTiles(2);
+    },
+    // The next round button
+    nextRoundButton: null,
+    /**
+     * @description Handles the next round button click
+     */
+    onNextRoundButtonClick: () => {
+        GUI.nextRoundButton.isVisible = false;
+
+        GUI.guiGoNext();
     }
 }
 
@@ -145,7 +203,7 @@ const createScene = () => {
     // Initialize the GUI
     GUI.texture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-    // Initial state of the block
+    // Initial state of the multiplier block
     GUI.multiplierBlock = new BABYLON.GUI.TextBlock();
     GUI.multiplierBlock.text = "xX.X"; // Placeholder text
     GUI.multiplierBlock.color = "white"; // Font color
@@ -154,6 +212,45 @@ const createScene = () => {
     GUI.multiplierBlock.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
     GUI.multiplierBlock.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
     GUI.texture.addControl(GUI.multiplierBlock);
+
+    // Initial state of the wager block
+    GUI.wagerBlock = new BABYLON.GUI.InputText();
+    GUI.wagerBlock.width = "300px";
+    GUI.wagerBlock.height = "50px";
+    GUI.wagerBlock.color = "white";
+    GUI.wagerBlock.background = "black";
+    GUI.wagerBlock.fontSize = 24;
+    GUI.wagerBlock.placeholderText = "Wager";
+    GUI.wagerBlock.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+    GUI.wagerBlock.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+    GUI.texture.addControl(GUI.wagerBlock);
+
+    // Initial state of the play button
+    GUI.playButton = BABYLON.GUI.Button.CreateSimpleButton("play_button", "Play");
+    GUI.playButton.width = "300px";
+    GUI.playButton.height = "50px";
+    GUI.playButton.color = "white";
+    GUI.playButton.background = "black";
+    GUI.playButton.fontSize = 24;
+    GUI.playButton.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+    GUI.playButton.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+    GUI.playButton.top = "100px";
+    GUI.playButton.onPointerUpObservable.add(GUI.onPlayButtonClick);
+    GUI.texture.addControl(GUI.playButton);
+
+    // Initial state of the next round button
+    GUI.nextRoundButton = BABYLON.GUI.Button.CreateSimpleButton("next_round_button", "Next Round");
+    GUI.nextRoundButton.width = "300px";
+    GUI.nextRoundButton.height = "50px";
+    GUI.nextRoundButton.color = "white";
+    GUI.nextRoundButton.background = "black";
+    GUI.nextRoundButton.fontSize = 24;
+    GUI.nextRoundButton.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+    GUI.nextRoundButton.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+    GUI.nextRoundButton.top = "320px";
+    GUI.nextRoundButton.isVisible = false;
+    GUI.nextRoundButton.onPointerUpObservable.add(GUI.onNextRoundButtonClick);
+    GUI.texture.addControl(GUI.nextRoundButton);
 
     return scene;
 }
